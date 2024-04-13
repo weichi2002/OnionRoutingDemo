4da7b84c6202e7b79713ad88910ddc879ed9d124
weichi2002 <wchou@fandm.edu>
2024-04-12 22:44:52 -0400

    Cmakelist

---
+++ build/CMakeFiles/onion_routing.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/onion_routing.dir/src/demo.cpp.o"
+  "CMakeFiles/onion_routing.dir/src/demo.cpp.o.d"
+  "onion_routing"
+  "onion_routing.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/onion_routing.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/onion_routing.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for onion_routing.
+# This may be replaced when dependencies are built.
